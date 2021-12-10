f028ebbc0d899105d1e7fcf7754fb56f80d89146
Cade Gordon <cadegordon093@gmail.com>
2021-12-10 11:39:34 -0600

    added sprite component class

--- raygame/MoveComponent.cpp
+++ raygame/MoveComponent.cpp
@@ -9,4 +9,6 @@ void MoveComponent::update(float deltaTime)
 {
 	MathLibrary::Vector2 position = getOwner()->getTransform()->getLocalPosition();
 	MathLibrary::Vector2 movement = MathLibrary::Vector2((position.x * m_maxSpeed * deltaTime * getOwner()->getTransform()->getForward().x), position.y * m_maxSpeed * deltaTime * getOwner()->getTransform()->getForward().y);
+
+	getOwner()->getTransform()->setLocalPosition(movement);
 }
