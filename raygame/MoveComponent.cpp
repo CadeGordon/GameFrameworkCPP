8e7e673892699d7f8af7bec2b2724d552db3f3b0
Cade Gordon <cadegordon093@gmail.com>
2021-12-10 11:30:58 -0600

    move component complete

--- raygame/MoveComponent.cpp
+++ raygame/MoveComponent.cpp
@@ -1,1 +1,12 @@
 #include "MoveComponent.h"
+
+MoveComponent::MoveComponent(Actor* owner, float maxSpeed) : Component::Component(owner, "Move Component")
+{
+	m_maxSpeed = maxSpeed;
+}
+
+void MoveComponent::update(float deltaTime)
+{
+	MathLibrary::Vector2 position = getOwner()->getTransform()->getLocalPosition();
+	MathLibrary::Vector2 movement = MathLibrary::Vector2((position.x * m_maxSpeed * deltaTime * getOwner()->getTransform()->getForward().x), position.y * m_maxSpeed * deltaTime * getOwner()->getTransform()->getForward().y);
+}
